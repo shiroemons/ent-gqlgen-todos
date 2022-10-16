1db271862165b947c289b13767739d8565ecba5c
shiroemons <shiroemons@gmail.com>
2022-10-16 17:23:05 +0900

    Relay Node Interface

--- ent.resolvers.go
+++ ent.resolvers.go
@@ -5,19 +5,18 @@ package todo
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/shiroemons/ent-gqlgen-todos/ent"
 )
 
 // Node is the resolver for the node field.
 func (r *queryResolver) Node(ctx context.Context, id int) (ent.Noder, error) {
-	panic(fmt.Errorf("not implemented: Node - node"))
+	return r.client.Noder(ctx, id)
 }
 
 // Nodes is the resolver for the nodes field.
 func (r *queryResolver) Nodes(ctx context.Context, ids []int) ([]ent.Noder, error) {
-	panic(fmt.Errorf("not implemented: Nodes - nodes"))
+	return r.client.Noders(ctx, ids)
 }
 
 // Todos is the resolver for the todos field.
